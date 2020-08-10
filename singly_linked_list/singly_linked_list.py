d9394c5f9eb6441f0dd6ea4ccfa90d96678d8c1a
Zebfred <zebfred22@gmail.com>
2020-08-10 16:14:19 -0700

    CS34 Day 1 MVP

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,92 +1,124 @@
 class Node:
-    def __init__(self, value, next_node = None):
-        self.value     = value
-        self.next_node = next_node
+    def __init__(me, value=None, next_node=None):
+        # value at this linked list node
+        me.value = value 
+        #reference to the next node in the list
+        me.next_node = next_node 
 
-    def get_value(self):
-        return self.value
+    def get_value(me):
+        return me.value
 
-    def get_next(self):
-        return self.next_node
+    def get_next(me):
+        return me.next_node
 
-    def set_next(self, new_next):
-        self.next_node = new_next
+    def set_next(me, new_next):
+        #set this node's next_node reference to the passed in node 
+        me.next_node = new_next
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+    def __init__(me): 
+        #reference to the head of the list
+        me.head = None 
+        #reference to the tail of the list 
+        me.tail = None 
 
-    # fill
-    def add_to_head(self, value):
-        new_node = Node(value)
-
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
+    def add_to_tail(me, value):
+        #wrap the input value in a node
+        new_node = Node(value, None)
+        #check if there is no head (i.e., the list is empty?)               
+        if not me.head:
+            #if the list is initially empty, set both head and tail to the new node
+            me.head = new_node
+            me.tail = new_node
+        #we have a non-empty list, add the new node to the tail
         else:
-            new_node.set_next(self.head)
-            self.head = new_node
+            #set the current tail's next reference to our new node 
+            me.tail.set_next      
+            #set the list's tail reference to the new node
+            me.tail = new_node
 
-    # append
-    def add_to_tail(self, value):
-        new_node = Node(value)
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.set_next(new_node)
-            self.tail = new_node
+    def remove_head(me):
+        #return None if there is no head (i.e. the list is empty)
+        if not me.head()
+            return None
+        #if head has no next, then we have a single element in our list
+        if not me.head.get_next():
+            #get a reference to the head
+            head = me.head
+            #delete the list's head reference
+            me.head = None
+            #also make sure the tail reference doesn't refer to anything 
+            me.tail = None
+            #return value
+            head.get_value()
+        #otherwise we have more than one element in our list
+        value = me.head.get_value()
+        #set the head reference to the current head's next node in the list
+        me.head = me.head.get_next()
+        return value
 
-    # pop (Always hated that method name, it doesn't describe what it's doing at all)
-    # You can't remove this. You can't remove anything with the way they're doing this because you can't actually delete anything.
-    # It's trying to get some value popped out, and when it does that after removing something it - for some reason - gets None when it should be getting some value.
-    # So instead of using it like a regular list and getting some value from the end - because it's there - I can't.
-    # It's just magically invisible.
-    # Even though I tell it to go to the next.
-    # But the current one is None, because it's deleted. You can't get the next one because the current one is lost.
-    # But where is it? There's still a tail!
-    # All of this is way too much abstraction, I can't just fucking look at it like a regular list and I can't just print out everything
-    # Just use a list and accept the loss in efficiency, it's not worth the sacrifice in legibility.
-    def remove_head(self):
+    def remove_tail(self):
         if not self.head:
             return None
-        else:
-            value     = self.head.get_value()
-            self.head = self.head.get_next()
 
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
             return value
 
-    # max
-    def get_max(self):
-        if not self.head and not self.tail:
-            return None
-        current = self.head
-        maximum = current.value
+        current = self.head 
 
-        while current != None:
-            if current.value > maximum:
-                maximum = current.value
+        while current.get_next() is not self.tail:
             current = current.get_next()
-        return maximum
 
-    # has
+        value = self.tail.get_value()
+        self.tail = current 
+        return value
+
     def contains(self, value):
-        current = self.head
+        if not self.head:
+            return False
 
-        while current != None:
-            if current.value == value:
+            # Recursive solution
+        # def search(node):
+        #   if node.get_value() == value:
+        #     return True
+        #   if not node.get_next():
+        #     return False
+        #   return search(node.get_next())
+        # return search(self.head)
+
+        #get a reference to the node we're currently at; update this as we traverse the list 
+        current = self.head 
+        #check to see if we're at a valide node
+        while current:
+            #return True if the current value we're looking at matches our target value
+            if current.get_value() == value:
                 return True
-            else:
-                current = current.get_next()
+            # update our current node to the current node's next node
+            current = current.get_next()
+        #if we've gotten here, then the target node isn't in our list 
         return False
 
+    def get_max(self):
+        if not self.head:
+            return None
 
-# ll = LinkedList()
-# ll.add_to_tail(3)
-# ll.add_to_tail(5)
-# ll.add_to_tail(9)
-# ll.add_to_tail(11)
-# print(ll.head.get_value())
-# print(ll.remove_head())
-# print(ll.head.get_value())
+     #reference to the largest value we've seen so far
+        max_value = self.head.get_value()
+        #reference to our current node as we traverse the list
+        current = self.head.get_next()
+        #check to see if we're still as a valid list node
+        while current:
+            # check to see if the current value is greater than the max_value
+            if current.get_value() > max_value:
+                #if so, update our max_calue cariable
+                max_value = current.get_value()
+               #update the current node to the next node in the list
+               current = current.get_next() 
+        return max_value        
+                
+
+
+
--- stack/stack.py
+++ stack/stack.py
@@ -11,15 +11,40 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 class Stack:
-    def __init__(self):
-        self.size = 0
-        # self.storage = ?
+def __init__(self):
+    self.size = 0
+    # self.storage = ?
+    self.storage = LinkedList()    
 
-    def __len__(self):
-        pass
+def __len__(self):
+    pass
+    count = 0
+    head = self.storage.head
+    tail = self.storage.tail
+    done = False 
+    while done is False:
+        if count == 0 and head is None:
+            done = True 
+        elif count == 0 and head.get_next is None:
+            count += 1
+            done = True 
+        elif head is not tail:
+            head.get_next()
+            count += 1
+        elif:
+            count += 1
+            done = True
+    return count                    
 
-    def push(self, value):
-        pass
 
-    def pop(self):
-        pass
+
+def push(self, value):
+    pass
+    self.storage.add_to_tail(value)
+
+def pop(self):
+    pass
+    return self.storage.remove_tail()
+
+
+
